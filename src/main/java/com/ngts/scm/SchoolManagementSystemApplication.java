912f5986a62e51fa04c2abbd3498488a3799a5f1
Ramadoss <ramadosspgm@gmail.com>
2023-09-09 22:14:04 +0530

    eureka client removed

--- src/main/java/com/ngts/scm/SchoolManagementSystemApplication.java
+++ src/main/java/com/ngts/scm/SchoolManagementSystemApplication.java
@@ -2,11 +2,9 @@ package com.ngts.scm;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@EnableDiscoveryClient
 @ComponentScan(basePackages = {"com.ngts.common.*","com.ngts.scm.*"})
 public class SchoolManagementSystemApplication {
 
