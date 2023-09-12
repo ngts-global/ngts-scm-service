4fc1694577be8db5e6a83bf8b9a85afa0e23b5ec
Ramadoss <ramadosspgm@gmail.com>
2023-09-12 21:33:44 +0530

    Redis events published

--- src/main/java/com/ngts/common/redis/events/impl/StudentEventServiceImpl.java
+++ src/main/java/com/ngts/common/redis/events/impl/StudentEventServiceImpl.java
@@ -5,7 +5,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.listener.ChannelTopic;
+import org.springframework.stereotype.Component;
 
+@Component
 public class StudentEventServiceImpl implements StudentEventService{
 
     @Autowired
@@ -16,6 +18,7 @@ public class StudentEventServiceImpl implements StudentEventService{
     private ChannelTopic channelTopic;
 
     public String publishEvent(StudentEventObj eventObj){
+        System.out.println("publishing events in Redis ...." + eventObj.getEmail());
         return redisTemplate.convertAndSend(channelTopic.getTopic(), eventObj).toString();
     }
 }
