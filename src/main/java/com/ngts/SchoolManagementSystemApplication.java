d924b1fa7373e28b2421756169a482ced7330422
Ramadoss <ramadosspgm@gmail.com>
2024-01-03 21:49:43 +0530

    chat service
    Simple chat service is included

--- src/main/java/com/ngts/SchoolManagementSystemApplication.java
+++ src/main/java/com/ngts/SchoolManagementSystemApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = {"com.ngts.common.*","com.ngts.scm.*","com.ngts.common.*", "com.ngts.auth.*"})
+@ComponentScan(basePackages = {"com.ngts.chat1.*","com.ngts.common.*","com.ngts.scm.*","com.ngts.common.*", "com.ngts.auth.*"})
 public class SchoolManagementSystemApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/ngts/chat1/controller/MessageController.java
@@ -0,0 +1,25 @@
+package com.ngts.chat1.controller;
+
+import com.ngts.chat1.entity.MessageModel;
+import com.ngts.chat1.repository.UserStorage;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.DestinationVariable;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MessageController {
+
+    @Autowired
+    private SimpMessagingTemplate simpMessagingTemplate;
+
+    @MessageMapping("/chat/{to}")
+    public void sendMessage(@DestinationVariable String to, MessageModel message) {
+        System.out.println("handling send message: " + message + " to: " + to);
+        boolean isExists = UserStorage.getInstance().getUsers().contains(to);
+        if (isExists) {
+            simpMessagingTemplate.convertAndSend("/topic/messages/" + to, message);
+        }
+    }
+}
